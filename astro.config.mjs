4518e72175f11d3a3d5090f99db78da06974ba95
sergio <elmatadorsergio1234@gmail.com>
2024-05-23 19:11:02 -0400

    cambios para deply en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  output: 'server',
+  adapter: vercel(),
 });
